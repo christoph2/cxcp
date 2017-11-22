036c4fa274314b7f375eab635e21e154bcd9c4de
Christoph Schueler <cpu12.gems@googlemail.com>
2017-11-22 11:49:23 +0000

    Implement basic structure.

--- main.c
+++ main.c
@@ -1,11 +1,49 @@
 
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "xcp.h"
 #include "comm.h"
 
 
+void sig_handler(int s){
+    printf("Caught signal %d\n",s);
+    XcpComm_DeInit();
+    exit(1);
+}
+
 int main()
 {
-  XcpComm_Init();  
-  return 0;
+
+    //setvbuf(stdout, NULL, _IONBF, 0);
+
+#if 0
+    struct sigaction sigIntHandler;
+
+    sigIntHandler.sa_handler = my_handler;
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = 0;
+
+    sigaction(SIGINT, &sigIntHandler, NULL);
+#endif
+    //if (signal(SIGINT, sig_handler) == SIG_ERR) {
+    //  printf("\ncan't catch SIGINT\n");
+    //}
+
+    XcpComm_Init();
+    //for(;;) {
+    //}
+
+    printf("Starting XCP task...\n");
+    fflush(stdout);
+//#if 0
+    for (;;) {
+        XcpComm_Task();
+    }
+//#endif
+    XcpComm_DeInit();
+
+    return 0;
 }
+
