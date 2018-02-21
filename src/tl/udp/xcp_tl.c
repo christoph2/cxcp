bfa5a2131b481e0d63e9f9c9e3120920049ca38c
Christoph Schueler <cpu12.gems@googlemail.com>
2018-02-21 14:43:15 +0100

    check 'QueryPerformanceCounter'

--- src/tl/udp/xcp_tl.c
+++ src/tl/udp/xcp_tl.c
@@ -1,3 +1,27 @@
+/*
+ * pySART - Simplified AUTOSAR-Toolkit for Python.
+ *
+ * (C) 2007-2018 by Christoph Schueler <github.com/Christoph2,
+ *                                      cpu12.gems@googlemail.com>
+ *
+ * All Rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * s. FLOSS-EXCEPTION.txt
+ */
 
 //#define WIN32_LEAN_AND_MEAN
 
@@ -115,6 +139,24 @@ void XcpComm_Init(void)
 
     DWORD dwTimeAdjustment = 0, dwTimeIncrement = 0, dwClockTick;
     BOOL fAdjustmentDisabled = TRUE;
+
+    LARGE_INTEGER StartingTime, EndingTime, ElapsedMicroseconds;
+    LARGE_INTEGER Frequency;
+
+    QueryPerformanceFrequency(&Frequency);
+    printf("Freq: %lu\n", Frequency.QuadPart);
+
+    QueryPerformanceCounter(&StartingTime);
+
+// Activity to be timed
+    Sleep(2000);
+
+    QueryPerformanceCounter(&EndingTime);
+    ElapsedMicroseconds.QuadPart = EndingTime.QuadPart - StartingTime.QuadPart;
+    printf("ETA: %f\n", (float)ElapsedMicroseconds.QuadPart / (float)Frequency.QuadPart);
+//////
+//////
+//////
     GetSystemTimeAdjustment(&dwTimeAdjustment, &dwTimeIncrement, &fAdjustmentDisabled);
     printf("%ld %ld %ld\n", dwTimeAdjustment, dwTimeIncrement, fAdjustmentDisabled);
 
