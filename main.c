e00781c902841e50e110ac2bfb01e5166fde7e25
Christoph Schueler <cpu12.gems@googlemail.com>
2018-03-02 15:12:04 +0100

    Add some test code.

--- main.c
+++ main.c
@@ -6,12 +6,47 @@
 #include "xcp.h"
 #include "xcp_tl.h"
 
+#include <stdio.h>
+#include <Windows.h>
+#include <DbgHelp.h>
+#pragma comment( lib, "dbghelp.lib" )
 
-void sig_handler(int s){
-    printf("Caught signal %d\n",s);
-    XcpTl_DeInit();
-    exit(1);
+
+// https://stackoverflow.com/questions/4308996/finding-the-address-range-of-the-data-segment
+void print_PE_section_info(HANDLE hModule) // hModule is the handle to a loaded Module (.exe or .dll)
+{
+   // get the location of the module's IMAGE_NT_HEADERS structure
+   IMAGE_NT_HEADERS *pNtHdr = ImageNtHeader(hModule);
+
+   // section table immediately follows the IMAGE_NT_HEADERS
+   IMAGE_SECTION_HEADER *pSectionHdr = (IMAGE_SECTION_HEADER *)(pNtHdr + 1);
+
+   const char* imageBase = (const char*)hModule;
+   char scnName[sizeof(pSectionHdr->Name) + 1];
+   scnName[sizeof(scnName) - 1] = '\0'; // enforce nul-termination for scn names that are the whole length of pSectionHdr->Name[]
+
+   for (int scn = 0; scn < pNtHdr->FileHeader.NumberOfSections; ++scn)
+   {
+      // Note: pSectionHdr->Name[] is 8 bytes long. If the scn name is 8 bytes long, ->Name[] will
+      // not be nul-terminated. For this reason, copy it to a local buffer that's nul-terminated
+      // to be sure we only print the real scn name, and no extra garbage beyond it.
+      strncpy(scnName, (const char*)pSectionHdr->Name, sizeof(pSectionHdr->Name));
+
+      printf("  Section %3d: %p...%p %-10s (%u bytes)\n",
+         scn,
+         imageBase + pSectionHdr->VirtualAddress,
+         imageBase + pSectionHdr->VirtualAddress + pSectionHdr->Misc.VirtualSize - 1,
+         scnName,
+         pSectionHdr->Misc.VirtualSize);
+      ++pSectionHdr;
+   }
 }
+
+// For demo purpopses, create an extra constant data section whose name is exactly 8 bytes long (the max)
+#pragma const_seg(".t_const") // begin allocating const data in a new section whose name is 8 bytes long (the max)
+const char const_string1[] = "This string is allocated in a special const data segment named \".t_const\".";
+#pragma const_seg() // resume allocating const data in the normal .rdata section
+
 
 
 #pragma bss_seg(push, /*stack1,*/ ".arbeitsseite")
@@ -23,6 +58,7 @@ int bss_normal;
 
 int main()
 {
+    print_PE_section_info(GetModuleHandle(NULL));
     Xcp_Init();
 
     //DBG_PRINT("Starting XCP task...\n");
