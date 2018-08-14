96f2a1e35c7debdae8a08edc29694f631bfc6043
Christoph Schueler <cpu12.gems@googlemail.com>
2018-08-14 10:22:20 +0200

    Cleanup 'main.c'

--- main.c
+++ main.c
@@ -5,44 +5,6 @@
 
 #include "xcp_ow.h"
 
-#include <stdio.h>
-#include <Windows.h>
-#include <DbgHelp.h>
-#pragma comment( lib, "dbghelp.lib" )
-
-#define _CRT_SECURE_NO_WARNINGS
-
-
-// https://stackoverflow.com/questions/4308996/finding-the-address-range-of-the-data-segment
-void print_PE_section_info(HANDLE hModule) // hModule is the handle to a loaded Module (.exe or .dll)
-{
-   int scn;
-   // get the location of the module's IMAGE_NT_HEADERS structure
-   IMAGE_NT_HEADERS *pNtHdr = ImageNtHeader(hModule);
-
-   // section table immediately follows the IMAGE_NT_HEADERS
-   IMAGE_SECTION_HEADER *pSectionHdr = (IMAGE_SECTION_HEADER *)(pNtHdr + 1);
-
-   const char* imageBase = (const char*)hModule;
-   char scnName[sizeof(pSectionHdr->Name) + 1];
-   scnName[sizeof(scnName) - 1] = '\0'; // enforce nul-termination for scn names that are the whole length of pSectionHdr->Name[]
-
-   for (scn = 0; scn < pNtHdr->FileHeader.NumberOfSections; ++scn)
-   {
-      // Note: pSectionHdr->Name[] is 8 bytes long. If the scn name is 8 bytes long, ->Name[] will
-      // not be nul-terminated. For this reason, copy it to a local buffer that's nul-terminated
-      // to be sure we only print the real scn name, and no extra garbage beyond it.
-      strncpy_s(scnName, IMAGE_SIZEOF_SHORT_NAME, (const char*)pSectionHdr->Name, sizeof(pSectionHdr->Name));
-
-      printf("  Section %3d: %p...%p %-15s (%u bytes)\n",
-         scn,
-         imageBase + pSectionHdr->VirtualAddress,
-         imageBase + pSectionHdr->VirtualAddress + pSectionHdr->Misc.VirtualSize - 1,
-         scnName,
-         pSectionHdr->Misc.VirtualSize);
-      ++pSectionHdr;
-   }
-}
 
 // For demo purpopses, create an extra constant data section whose name is exactly 8 bytes long (the max)
 #pragma const_seg(".t_const") // begin allocating const data in a new section whose name is 8 bytes long (the max)
@@ -70,11 +32,10 @@ int main()
 {
     pagTest();
 
-    XcpOnCan_Init();
+    //XcpOnCan_Init();
 
     XcpOw_MapFileOpen(FNAME, &a2lFile);
 
-    //print_PE_section_info(GetModuleHandle(NULL));
     Xcp_Init();
     DBG_PRINT1("Starting XCP task...\n");
     fflush(stdout);
