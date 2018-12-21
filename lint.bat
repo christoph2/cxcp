d5b73e4568ec0e18be774fae99ac256f2d03a2d0
Christoph Schueler <cpu12.gems@googlemail.com>
2018-12-21 16:03:25 +0100

    Update PC-LINT options

--- lint.bat
+++ lint.bat
@@ -1,1 +1,1 @@
-lint-nt  +v -i"c:\lint" -i"inc" -i"tests" -D_WIN32 -b loptions.lnt project.lnt temp.lnt  %1 %2 %3 %4 %5 %6 %7 %8 %9 | less
+lint-nt  +v -i"c:\lint" -i"inc" -i"." -D_WIN32 -b loptions.lnt project.lnt temp.lnt  %1 %2 %3 %4 %5 %6 %7 %8 %9
