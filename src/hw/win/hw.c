5c9e7d7db5e8f83eb19fa51064ec6456a18ce731
Christoph Schueler <cpu12.gems@googlemail.com>
2018-12-26 10:34:49 +0100

    Add XcpHw_Mainfunction()

--- src/hw/win/hw.c
+++ src/hw/win/hw.c
@@ -64,6 +64,8 @@ static HwStateType HwState = {0};
 
 void exitFunc(void);
 
+void Win_ErrorMsg(char * const function, DWORD errorCode);
+
 void exitFunc(void)
 {
     printf("Exiting programm...\n");
@@ -78,7 +80,7 @@ void XcpHw_Init(void)
 {
     LARGE_INTEGER EndingTime, ElapsedMicroseconds;
 
-    //atexit(exitFunc);
+    atexit(exitFunc);
 
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
@@ -138,3 +140,90 @@ uint32_t XcpHw_GetTimerCounter(void)
 #error Timestamp-size not supported.
 #endif // XCP_DAQ_TIMESTAMP_SIZE
 }
+
+#if 0
+void Win_ErrorMsg(char * const fun, DWORD errorCode)
+{
+    //LPWSTR buffer = NULL;
+    char * buffer = XCP_NULL;
+
+    FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, XCP_NULL, errorCode, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), &buffer, 0, XCP_NULL);
+    if (buffer != XCP_NULL) {
+        fprintf(stderr, "[%s] failed with: [%d] %s", fun, errorCode, buffer);
+        LocalFree((HLOCAL)buffer);
+    } else {
+        //DBG_PRINT("FormatMessage failed!\n");
+    }
+}
+
+void Win_Error(char * const function, DWORD errorCode)
+{
+    char * szBuf;
+    LPWSTR  lpMsgBuf;
+    WORD len, idx;
+
+    FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, NULL, errorCode,
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR) &lpMsgBuf, 0, NULL );
+
+    len = lstrlen(lpMsgBuf) + 1;
+    szBuf = (char *)_alloca((len * sizeof(lpMsgBuf[0])));
+    for (idx = 0; idx < len; ++idx) {
+        szBuf[idx] = lpMsgBuf[idx];
+    }
+    printf("%s failed with error %d: %s", function, errorCode, szBuf);
+
+    _freea(szBuf);
+    LocalFree(lpMsgBuf);
+}
+#endif
+
+void XcpHw_Mainfunction(bool * finished)
+{
+    HANDLE hStdin;
+    DWORD cNumRead, fdwMode, idx;
+    INPUT_RECORD irInBuf[128];
+    KEY_EVENT_RECORD key;
+
+    *finished = XCP_FALSE;
+
+    hStdin = GetStdHandle(STD_INPUT_HANDLE);
+    if (hStdin == INVALID_HANDLE_VALUE) {
+        Win_ErrorMsg("GetStdHandle", GetLastError());
+    }
+
+    fdwMode = ENABLE_WINDOW_INPUT | ENABLE_MOUSE_INPUT;
+    if (!SetConsoleMode(hStdin, fdwMode)) {
+        Win_ErrorMsg("SetConsoleMode", GetLastError());
+    }
+
+    if (!GetNumberOfConsoleInputEvents (hStdin, &cNumRead)) {
+        Win_ErrorMsg("PeekConsoleInput", GetLastError());
+    } else {
+        if (cNumRead) {
+            if (!ReadConsoleInput(hStdin, irInBuf, 128, &cNumRead)) {
+                Win_ErrorMsg("ReadConsoleInput", GetLastError());
+            }
+            for (idx = 0; idx < cNumRead; ++idx) {
+            switch(irInBuf[idx].EventType) {
+                    case KEY_EVENT:
+                        key = irInBuf[idx].Event.KeyEvent;
+                        printf("KeyEvent: %x %x %x %u\n", key.wVirtualKeyCode, key.wVirtualScanCode, key.dwControlKeyState, key.bKeyDown);
+                        if (key.bKeyDown) {
+                            if (key.wVirtualKeyCode == VK_ESCAPE) {
+                                *finished = XCP_TRUE;
+                                return;
+                            }
+                            if (key.wVirtualKeyCode == VK_F9) {
+//                                printf("\tF9\n");
+                            }
+                        }
+                        break;
+                    default:
+                        //MyErrorExit("unknown event type");
+                        break;
+                }
+            }
+        }
+    }
+}
+
