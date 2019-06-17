923c1eee7c0967b51e62fe0827c9d4afcfea01ac
Christoph Schueler <cpu12.gems@googlemail.com>
2019-06-17 10:31:03 +0200

    Add simwin example

---
+++ examples/simwin/main.c
@@ -0,0 +1,203 @@
+
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "xcp.h"
+#include "xcp_hw.h"
+#include "flsemu.h"
+
+
+
+#pragma bss_seg(push, /*stack1,*/ ".arbeitsseite")
+int in_der_arbeitsseite;
+uint8_t calram[255];
+#pragma bss_seg(pop/*, stack1*/)
+int bss_normal;
+
+void XcpOnCan_Init(void);
+
+#define SIZE    (4096)
+uint8_t puffer[SIZE];
+
+void XcpHw_MainFunction(bool * finished);
+void XcpTl_SetOptions(XcpHw_OptionsType const * options);
+
+void AppTask(void);
+
+#define FLS_SECTOR_SIZE (256)
+
+#define FLS_PAGE_ADDR           ((uint16_t)0x8000U)
+#define FLS_PAGE_SIZE           ((uint32_t)0x4000U)
+
+
+static FlsEmu_SegmentType S12D512_PagedFlash = {
+    "XCPSIM_Flash",
+    FLSEMU_KB(512),
+    2,
+    FLS_SECTOR_SIZE,
+    FLS_PAGE_SIZE,
+    4,
+    0x8000,
+    XCP_NULL,
+};
+
+static FlsEmu_SegmentType S12D512_EEPROM = {
+    "XCPSIM_EEPROM",
+    FLSEMU_KB(4),
+    2,
+    4,
+    FLSEMU_KB(4),
+    1,
+    0x4000,
+    XCP_NULL,
+};
+
+
+static FlsEmu_SegmentType const * segments[] = {
+    &S12D512_PagedFlash,
+    &S12D512_EEPROM,
+};
+
+static const FlsEmu_ConfigType FlsEmu_Config = {
+    2,
+    segments,
+};
+
+void memimnfo(void * ptr)
+{
+    /*
+    ** mainly a debugging/bug reporting aid.
+    */
+
+    MEMORY_BASIC_INFORMATION info;
+    SYSTEM_INFO si;
+    size_t res;
+
+    GetSystemInfo(&si);
+    res = VirtualQuery(ptr, &info, sizeof(MEMORY_BASIC_INFORMATION));
+    if (!res) {
+        Win_ErrorMsg("memimnfo::VirtualQuery()", GetLastError());
+    } else {
+        printf("%p %p %d %08x %08x %08x\n", info.BaseAddress, info.AllocationBase, info.RegionSize / 1024,
+               info.AllocationProtect, info.Protect, info.Type);
+    }
+}
+
+
+
+
+int main(void)
+{
+    bool finished;
+    uint8_t * ptr;
+    XcpHw_OptionsType options;
+
+    XcpHw_GetCommandLineOptions(&options);
+    XcpTl_SetOptions(&options);
+
+
+    FlsEmu_Init(&FlsEmu_Config);
+
+    FlsEmu_ErasePage(0, 3);
+    ptr = FlsEmu_BasePointer(0);
+    FillMemory(ptr + 0x180, 0x80, 0xaa);
+
+    Xcp_Init();
+	XcpTl_DisplayInfo();
+
+    for (;;) {
+        Xcp_MainFunction();
+        XcpTl_MainFunction();
+
+        XcpHw_MainFunction(&finished);
+        if (finished) {
+            break;
+        }
+        AppTask();
+        XcpDaq_MainFunction();
+    }
+
+    FlsEmu_DeInit();
+
+    XcpTl_DeInit();
+
+    return 0;
+}
+
+
+bool Xcp_HookFunction_GetSeed(uint8_t resource, Xcp_1DArrayType * result)
+{
+    static const uint8_t seed[] = {0x11, 0x22, 0x33, 0x44};
+    result->length = 4;
+    result->data = (uint8_t*)&seed;
+
+    return XCP_TRUE;
+}
+
+
+bool Xcp_HookFunction_Unlock(uint8_t resource, Xcp_1DArrayType const * key)
+{
+    static const uint8_t secret[] = {0x55, 0x66, 0x77, 0x88};
+
+//    printf("\tKEY [%u]: ", key->length);
+//    Xcp_Hexdump(key->data, key->length);
+
+    return Xcp_MemCmp(&secret, key->data, XCP_ARRAY_SIZE(secret));
+}
+
+
+bool Xcp_HookFunction_CheckMemoryAccess(Xcp_MtaType mta, Xcp_MemoryAccessType access, bool programming)
+{
+    return XCP_TRUE;
+}
+
+#if 0
+bool Xcp_HookFunction_GetId(uint8_t idType)
+{
+    if (idType == 4) {
+        Xcp_SetMta(Xcp_GetNonPagedAddress(a2lFile.view.mappingAddress));
+        Xcp_Send8(8, 0xff, 0, 0, 0, XCP_LOBYTE(XCP_LOWORD(a2lFile.size)), XCP_HIBYTE(XCP_LOWORD(a2lFile.size)), XCP_LOBYTE(XCP_HIWORD(a2lFile.size)), XCP_HIBYTE(XCP_HIWORD(a2lFile.size)));
+        return XCP_TRUE;
+    }
+    return XCP_FALSE;
+}
+#endif // 0
+
+typedef struct {
+    uint8_t value;
+    bool down;
+} triangle_type;
+
+triangle_type triangle = {0};
+
+
+void AppTask(void)
+{
+    static uint32_t currentTS = 0UL;
+    static uint32_t previousTS = 0UL;
+    static uint16_t ticker = 0;
+
+    currentTS = XcpHw_GetTimerCounter() / 1000;
+    if (currentTS >= (previousTS + 10)) {
+        //printf("T [%u::%lu]\n", currentTS, XcpHw_GetTimerCounter() / 1000);
+
+        if ((ticker % 3) == 0) {
+            if (triangle.down) {
+                triangle.value--;
+                if (triangle.value == 0) {
+                    triangle.down = XCP_FALSE;
+                }
+            } else {
+                triangle.value++;
+                if (triangle.value >= 75) {
+                    triangle.down = XCP_TRUE;
+                }
+            }
+            //printf("\t\t\tTRI [%u]\n", triangle.value);
+            XcpDaq_TriggerEvent(1);
+        }
+        ticker++;
+        previousTS =  XcpHw_GetTimerCounter() / 1000;
+    }
+}
---
+++ examples/simwin/xcp_config.h
@@ -0,0 +1,200 @@
+/*
+ * BlueParrot XCP
+ *
+ * (C) 2007-2019 by Christoph Schueler <github.com/Christoph2,
+ *                                      cpu12.gems@googlemail.com>
+ *
+ * All Rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * s. FLOSS-EXCEPTION.txt
+ */
+
+#if !defined(__XCP_CONFIG_H)
+#define __XCP_CONFIG_H
+
+
+#define XCP_STATION_ADDRESS                         (1)
+#define XCP_STATION_ID                              "description_of_test_ecu.a2l"
+
+#define XCP_BUILD_TYPE                              XCP_DEBUG_BUILD
+
+#define XCP_EXTERN_C_GUARDS                         XCP_OFF
+
+#define XCP_ENABLE_SLAVE_BLOCKMODE                  XCP_OFF
+#define XCP_ENABLE_MASTER_BLOCKMODE                 XCP_OFF
+
+#define XCP_ENABLE_STIM                             XCP_OFF
+
+#define XCP_CHECKSUM_METHOD                         XCP_CHECKSUM_METHOD_XCP_CRC_16_CITT
+#define XCP_CHECKSUM_CHUNKED_CALCULATION            XCP_ON
+#define XCP_CHECKSUM_CHUNK_SIZE                     (64)
+#define XCP_CHECKSUM_MAXIMUM_BLOCK_SIZE             (0)     /* 0 ==> unlimited */
+
+#define XCP_BYTE_ORDER                              XCP_BYTE_ORDER_INTEL
+#define XCP_ADDRESS_GRANULARITY                     XCP_ADDRESS_GRANULARITY_BYTE
+
+
+#define XCP_MAX_BS                                  (0)
+#define XCP_MIN_ST                                  (0)
+#define XCP_QUEUE_SIZE                              (0)
+
+#define XCP_DAQ_TIMESTAMP_UNIT                      (XCP_DAQ_TIMESTAMP_UNIT_1US)
+#define XCP_DAQ_TIMESTAMP_SIZE                      (XCP_DAQ_TIMESTAMP_SIZE_4)
+#define XCP_DAQ_PRESCALER_SUPPORTED                 XCP_OFF
+#define XCP_DAQ_ADDR_EXT_SUPPORTED                  XCP_OFF
+#define XCP_DAQ_BIT_OFFSET_SUPPORTED                XCP_OFF
+#define XCP_DAQ_PRIORITIZATION_SUPPORTED            XCP_OFF
+#define XCP_DAQ_ALTERNATING_SUPPORTED               XCP_OFF
+#define XCP_DAQ_CLOCK_ACCESS_ALWAYS_SUPPORTED       XCP_ON
+
+#define XCP_DAQ_MAX_EVENT_CHANNEL                   (3)
+#define XCP_DAQ_MULTIPLE_DAQ_LISTS_PER_EVENT_SUPPORTED  XCP_OFF
+
+
+/*
+** Resource Protection.
+*/
+#define XCP_PROTECT_CAL                             XCP_ON
+#define XCP_PROTECT_PAG                             XCP_ON
+#define XCP_PROTECT_DAQ                             XCP_ON
+#define XCP_PROTECT_STIM                            XCP_ON
+#define XCP_PROTECT_PGM                             XCP_ON
+
+
+/*
+**  Optional Services.
+*/
+    #define XCP_ENABLE_GET_COMM_MODE_INFO           XCP_ON
+    #define XCP_ENABLE_GET_ID                       XCP_ON
+    #define XCP_ENABLE_SET_REQUEST                  XCP_OFF
+    #define XCP_ENABLE_GET_SEED                     XCP_ON
+    #define XCP_ENABLE_UNLOCK                       XCP_ON
+    #define XCP_ENABLE_SET_MTA                      XCP_ON
+    #define XCP_ENABLE_UPLOAD                       XCP_ON
+    #define XCP_ENABLE_SHORT_UPLOAD                 XCP_ON
+    #define XCP_ENABLE_BUILD_CHECKSUM               XCP_ON
+    #define XCP_ENABLE_TRANSPORT_LAYER_CMD          XCP_OFF /* TODO: TL dependend include file! */
+    #define XCP_ENABLE_USER_CMD                     XCP_OFF
+
+#define XCP_ENABLE_CAL_COMMANDS                     XCP_ON
+
+    #define XCP_ENABLE_DOWNLOAD_NEXT                XCP_OFF
+    #define XCP_ENABLE_DOWNLOAD_MAX                 XCP_OFF
+    #define XCP_ENABLE_SHORT_DOWNLOAD               XCP_ON
+    #define XCP_ENABLE_MODIFY_BITS                  XCP_ON
+
+#define XCP_ENABLE_PAG_COMMANDS                     XCP_OFF
+
+    #define XCP_ENABLE_GET_PAG_PROCESSOR_INFO       XCP_OFF
+    #define XCP_ENABLE_GET_SEGMENT_INFO             XCP_OFF
+    #define XCP_ENABLE_GET_PAGE_INFO                XCP_OFF
+    #define XCP_ENABLE_SET_SEGMENT_MODE             XCP_OFF
+    #define XCP_ENABLE_GET_SEGMENT_MODE             XCP_OFF
+    #define XCP_ENABLE_COPY_CAL_PAGE                XCP_OFF
+
+#define XCP_ENABLE_DAQ_COMMANDS                     XCP_ON
+
+    #define XCP_ENABLE_GET_DAQ_CLOCK                XCP_ON
+    #define XCP_ENABLE_READ_DAQ                     XCP_OFF
+    #define XCP_ENABLE_GET_DAQ_PROCESSOR_INFO       XCP_ON
+    #define XCP_ENABLE_GET_DAQ_RESOLUTION_INFO      XCP_ON
+    #define XCP_ENABLE_GET_DAQ_LIST_INFO            XCP_OFF
+    #define XCP_ENABLE_GET_DAQ_EVENT_INFO           XCP_OFF
+    #define XCP_ENABLE_FREE_DAQ                     XCP_ON
+    #define XCP_ENABLE_ALLOC_DAQ                    XCP_ON
+    #define XCP_ENABLE_ALLOC_ODT                    XCP_ON
+    #define XCP_ENABLE_ALLOC_ODT_ENTRY              XCP_ON
+    #define XCP_ENABLE_WRITE_DAQ_MULTIPLE           XCP_OFF
+
+#define XCP_ENABLE_PGM_COMMANDS                     XCP_OFF
+
+    #define XCP_ENABLE_GET_PGM_PROCESSOR_INFO       XCP_OFF
+    #define XCP_ENABLE_GET_SECTOR_INFO              XCP_OFF
+    #define XCP_ENABLE_PROGRAM_PREPARE              XCP_OFF
+    #define XCP_ENABLE_PROGRAM_FORMAT               XCP_OFF
+    #define XCP_ENABLE_PROGRAM_NEXT                 XCP_OFF
+    #define XCP_ENABLE_PROGRAM_MAX                  XCP_OFF
+    #define XCP_ENABLE_PROGRAM_VERIFY               XCP_OFF
+
+/*
+**  Transport-Layer specific Options (may not apply to every Transport).
+*/
+#if defined(KVASER_CAN)
+    #define XCP_TRANSPORT_LAYER                         XCP_ON_CAN
+
+    #define XCP_MAX_CTO                                 (8)
+    #define XCP_MAX_DTO                                 (8)
+
+    #define XCP_TRANSPORT_LAYER_LENGTH_SIZE             (0)
+    #define XCP_TRANSPORT_LAYER_COUNTER_SIZE            (0)
+    #define XCP_TRANSPORT_LAYER_CHECKSUM_SIZE           (0)
+
+    #define XCP_ON_CAN_INBOUND_IDENTIFIER               (0x101 | XCP_CAN_EXT_IDENTIFIER)
+    #define XCP_ON_CAN_OUTBOUND_IDENTIFIER              (0x102 | XCP_CAN_EXT_IDENTIFIER)
+    #define XCP_ON_CAN_MAX_DLC_REQUIRED                 XCP_OFF
+    #define XCP_ON_CAN_BROADCAST_IDENTIFIER             (0x100)
+    #define XCP_ON_CAN_FREQ                             (canBITRATE_250K)
+    #define XCP_ON_CAN_BTQ                              (16)
+    #define XCP_ON_CAN_TSEG1                            (14)
+    #define XCP_ON_CAN_TSEG2                            (2)
+    #define XCP_ON_CAN_SJW                              (2)
+    #define XCP_ON_CAN_NOSAMP                           (1)
+#else
+    #define XCP_TRANSPORT_LAYER                         XCP_ON_ETHERNET
+
+    #define XCP_ENFORCE_CAN_RESTRICTIONS                XCP_OFF
+
+    #define XCP_MAX_CTO                                 (64)
+    #define XCP_MAX_DTO                                 (64)
+
+    #define XCP_TRANSPORT_LAYER_LENGTH_SIZE             (2)
+    #define XCP_TRANSPORT_LAYER_COUNTER_SIZE            (2)
+    #define XCP_TRANSPORT_LAYER_CHECKSUM_SIZE           (0)
+#endif // KVASER_CAN
+
+/*
+**  Customization Options.
+*/
+#define XCP_ENABLE_ADDRESS_MAPPER                   XCP_ON
+#define XCP_ENABLE_CHECK_MEMORY_ACCESS              XCP_ON
+
+#define XCP_ENABLE_GET_ID_HOOK                      XCP_OFF
+
+#define XCP_DAQ_LIST_TYPE                           uint8_t
+#define XCP_DAQ_ODT_TYPE                            uint8_t
+#define XCP_DAQ_ODT_ENTRY_TYPE                      uint8_t
+
+/*
+**  Platform specific Options.
+*/
+#define XCP_ENTER_CRITICAL()
+#define XCP_LEAVE_CRITICAL()
+#define XCP_TL_ENTER_CRITICAL()
+#define XCP_TL_LEAVE_CRITICAL()
+#define XCP_DAQ_ENTER_CRITICAL()
+#define XCP_DAQ_LEAVE_CRITICAL()
+#define XCP_STIM_ENTER_CRITICAL()
+#define XCP_STIM_LEAVE_CRITICAL()
+#define XCP_PGM_ENTER_CRITICAL()
+#define XCP_PGM_LEAVE_CRITICAL()
+#define XCP_CAL_ENTER_CRITICAL()
+#define XCP_CAL_LEAVE_CRITICAL()
+#define XCP_PAG_ENTER_CRITICAL()
+#define XCP_PAG_LEAVE_CRITICAL()
+
+
+#endif /* __XCP_CONFIG_H */
