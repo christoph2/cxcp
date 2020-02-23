b20caf93410ce2d6c0bd6658cbea7a0c0c7e9d0e
Christoph Schueler <cpu12.gems@googlemail.com>
2020-02-23 15:39:49 +0100

    Add dispatcher

--- src/tl/eth/linuxeth.c
+++ src/tl/eth/linuxeth.c
@@ -101,6 +101,10 @@ extern Xcp_PDUType Xcp_PduOut;
 static bool Xcp_EnableSocketOption(int sock, int option);
 static bool Xcp_DisableSocketOption(int sock, int option);
 
+static int Xcp_SetNonblocking(int fd);
+static void Xcp_AddFd(int epoll_fd, int fd);
+void lt_process(struct epoll_event* events, int number, int epoll_fd, int listen_fd);
+
 
 static  bool Xcp_EnableSocketOption(int sock, int option)
 {
@@ -135,43 +139,7 @@ static bool Xcp_DisableSocketOption(int sock, int option)
     return true;
 }
 
-void lt_process(struct epoll_event* events, int number, int epoll_fd, int listen_fd)
-{
-    char buf[BUFFER_SIZE];
-    int i;
-    for(i = 0; i < number; i++) //number: number of events ready
-    {
-        int sockfd = events[i].data.fd;
-        if(sockfd == listen_fd)  //If it is a file descriptor for listen, it indicates that a new customer is connected to
-        {
-            struct sockaddr_in client_address;
-            socklen_t client_addrlength = sizeof(client_address);
-            int connfd = accept(listen_fd, (struct sockaddr*)&client_address, &client_addrlength);
-            add_fd(epoll_fd, connfd, false);  //Register new customer connection fd to epoll event table, using lt mode
-        }
-        else if(events[i].events & EPOLLIN) //Readable with client data
-        {
-            // This code is triggered as long as the data in the buffer has not been read.This is what LT mode is all about: repeating notifications until processing is complete
-            printf("lt mode: event trigger once!\n");
-            memset(buf, 0, BUFFER_SIZE);
-            int ret = recv(sockfd, buf, BUFFER_SIZE - 1, 0);
-            if(ret <= 0)  //After reading the data, remember to turn off fd
-            {
-                close(sockfd);
-                continue;
-            }
-            printf("get %d bytes of content: \n", ret);
-//            hexdump(buf, ret);
-
-        }
-        else
-        {
-            printf("something unexpected happened!\n");
-        }
-    }
-}
-
-int SetNonblocking(int fd)
+static int Xcp_Set_Nonblocking(int fd)
 {
     int old_option = fcntl(fd, F_GETFL);
     int new_option = old_option | O_NONBLOCK;
@@ -179,14 +147,14 @@ int SetNonblocking(int fd)
     return old_option;
 }
 
-void add_fd(int epoll_fd, int fd)
+static void Xcp_AddFd(int epoll_fd, int fd)
 {
     struct epoll_event event;
     event.data.fd = fd;
     event.events = EPOLLIN; //Registering the fd is readable
 
     epoll_ctl(epoll_fd, EPOLL_CTL_ADD, fd, &event);  //Register the fd with the epoll kernel event table
-    SetNonblocking(fd);
+    Xcp_SetNonblocking(fd);
 }
 
 void XcpTl_Init(void)
@@ -260,7 +228,7 @@ void XcpTl_Init(void)
         exit(1);
     }
 
-    add_fd(epoll_fd, serverSockets[0]);
+    Xcp_AddFd(epoll_fd, serverSockets[0]);
 
     while(1)
     {
@@ -374,6 +342,61 @@ void XcpTl_RxHandler(void)
     }
 }
 
+void lt_process(struct epoll_event* events, int number, int epoll_fd, int listen_fd)
+{
+    char buf[BUFFER_SIZE];
+    int i;
+    int recv_len;
+    uint16_t dlc;
+
+    for(i = 0; i < number; i++) //number: number of events ready
+    {
+        int sockfd = events[i].data.fd;
+        if(sockfd == listen_fd) { //If it is a file descriptor for listen, it indicates that a new customer is connected to
+            struct sockaddr_in client_address;
+            socklen_t client_addrlength = sizeof(client_address);
+            int connfd = accept(listen_fd, (struct sockaddr*)&client_address, &client_addrlength);
+            printf("accepted()\n");
+            Xcp_AddFd(epoll_fd, connfd, false);  //Register new customer connection fd to epoll event table, using lt mode
+        } else if(events[i].events & EPOLLIN) { //Readable with client data
+            // This code is triggered as long as the data in the buffer has not been read.This is what LT mode is all about: repeating notifications until processing is complete
+            printf("lt mode: event trigger once!\n");
+            memset(buf, 0, BUFFER_SIZE);
+            int ret = recv(sockfd, buf, BUFFER_SIZE - 1, 0);
+            if(ret <= 0)  //After reading the data, remember to turn off fd
+            {
+                close(sockfd);
+                continue;
+            }
+            printf("get %d bytes of content: \n", ret);
+            recv_len = ret;
+//            hexdump(buf, ret);
+            if (recv_len > 0) {
+        #if XCP_TRANSPORT_LAYER_LENGTH_SIZE == 1
+                dlc = (uint16_t)buf[0];
+        #elif XCP_TRANSPORT_LAYER_LENGTH_SIZE == 2
+                dlc = XCP_MAKEWORD(buf[0], buf[1]);
+                //dlc = (uint16_t)*(buf + 0);
+        #endif // XCP_TRANSPORT_LAYER_LENGTH_SIZE
+                if (!XcpTl_Connection.connected || (XcpTl_VerifyConnection())) {
+                    Xcp_PduIn.len = dlc;
+                    Xcp_PduIn.data = buf + XCP_TRANSPORT_LAYER_BUFFER_OFFSET;
+                    Xcp_DispatchCommand(&Xcp_PduIn);
+                }
+                if (recv_len < 5) {
+                    DBG_PRINT2("Error: frame to short: %d\n", recv_len);
+                } else {
+
+                }
+                fflush(stdout);
+            }
+        }
+        else
+        {
+            printf("something unexpected happened!\n");
+        }
+    }
+}
 
 void XcpTl_TxHandler(void)
 {
@@ -457,394 +480,3 @@ void XcpTl_PrintConnectionInformation(void)
         Xcp_Options.ipv6 ? "IPv6" : "IPv4"
     );
 }
-
-// Simple
-#if 0
-#include <sys/socket.h>
-#include <sys/epoll.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <errno.h>
-#include <stdlib.h>
-#include <string.h>
-
-
-
-#define MAXLINE 5
-#define OPEN_MAX 100
-#define LISTENQ 20
-#define SERV_PORT 5000
-#define INFTIM 1000
-
-void setnonblocking(int sock)
-{
-    int opts;
-    opts=fcntl(sock,F_GETFL);
-    if(opts<0)
-    {
-        perror("fcntl(sock,GETFL)");
-        exit(1);
-    }
-    opts = opts|O_NONBLOCK;
-    if(fcntl(sock,F_SETFL,opts)<0)
-    {
-        perror("fcntl(sock,SETFL,opts)");
-        exit(1);
-    }
-}
-
-int main(int argc, char* argv[])
-{
-    int i, maxi, listenfd, connfd, sockfd,epfd,nfds, portnumber;
-    ssize_t n;
-    char line[MAXLINE];
-    socklen_t clilen;
-
-
-    if ( 2 == argc )
-    {
-        if( (portnumber = atoi(argv[1])) < 0 )
-        {
-            fprintf(stderr,"Usage:%s portnumber/a/n",argv[0]);
-            return 1;
-        }
-    }
-    else
-    {
-        fprintf(stderr,"Usage:%s portnumber/a/n",argv[0]);
-        return 1;
-    }
-
-
-
-    //Declare variables for the epoll_event structure, ev for registering events, and array for returning events to process
-
-    struct epoll_event ev,events[20];
-    //Generate epoll-specific file descriptors for processing accept s
-
-    epfd=epoll_create(256);
-    struct sockaddr_in clientaddr;
-    struct sockaddr_in serveraddr;
-    listenfd = socket(AF_INET, SOCK_STREAM, 0);
-    //Set socket to non-blocking
-
-    //setnonblocking(listenfd);
-
-    //Set the file descriptor associated with the event to be processed
-
-    ev.data.fd=listenfd;
-    //Set the type of event to process
-
-    ev.events=EPOLLIN|EPOLLET;
-    //ev.events=EPOLLIN;
-
-    //Register epoll events
-
-    epoll_ctl(epfd,EPOLL_CTL_ADD,listenfd,&ev);
-    bzero(&serveraddr, sizeof(serveraddr));
-    serveraddr.sin_family = AF_INET;
-    char *local_addr="127.0.0.1";
-    inet_aton(local_addr,&(serveraddr.sin_addr));//htons(portnumber);
-
-    serveraddr.sin_port=htons(portnumber);
-    bind(listenfd,(struct sockaddr *)&serveraddr, sizeof(serveraddr));
-    listen(listenfd, LISTENQ);
-    maxi = 0;
-    for ( ; ; ) {
-        //Waiting for the epoll event to occur
-
-        nfds=epoll_wait(epfd,events,20,500);
-        //Handle all events that occur
-
-        for(i=0;i<nfds;++i)
-        {
-            if(events[i].data.fd==listenfd)//If a new SOCKET user is detected to be connected to a bound SOCKET port, establish a new connection.
-
-            {
-                connfd = accept(listenfd,(struct sockaddr *)&clientaddr, &clilen);
-                if(connfd<0){
-                    perror("connfd<0");
-                    exit(1);
-                }
-                //setnonblocking(connfd);
-
-                char *str = inet_ntoa(clientaddr.sin_addr);
-                printf("accapt a connection from\n ");
-                //Setting file descriptors for read operations
-
-                ev.data.fd=connfd;
-                //Set Read Action Events for Annotation
-
-                ev.events=EPOLLIN|EPOLLET;
-                //ev.events=EPOLLIN;
-
-                //Register ev
-
-                epoll_ctl(epfd,EPOLL_CTL_ADD,connfd,&ev);
-            }
-            else if(events[i].events&EPOLLIN)//If the user is already connected and receives data, read in.
-
-            {
-                printf("EPOLLIN\n");
-                if ( (sockfd = events[i].data.fd) < 0)
-                    continue;
-                if ( (n = read(sockfd, line, MAXLINE)) < 0) {
-                    if (errno == ECONNRESET) {
-                        close(sockfd);
-                        events[i].data.fd = -1;
-                    } else
-                        printf("readline error\n");
-                } else if (n == 0) {
-                    close(sockfd);
-                    events[i].data.fd = -1;
-                }
-                if(n<MAXLINE-2)
-                    line[n] = '\0';
-
-                //Setting file descriptors for write operations
-
-                ev.data.fd=sockfd;
-                //Set Write Action Events for Annotation
-
-                ev.events=EPOLLOUT|EPOLLET;
-                //Modify the event to be handled on sockfd to EPOLLOUT
-
-                //epoll_ctl(epfd,EPOLL_CTL_MOD,sockfd,&ev);
-
-            }
-            else if(events[i].events&EPOLLOUT) // If there is data to send
-
-            {
-                sockfd = events[i].data.fd;
-                write(sockfd, line, n);
-                //Setting file descriptors for read operations
-
-                ev.data.fd=sockfd;
-                //Set Read Action Events for Annotation
-
-                ev.events=EPOLLIN|EPOLLET;
-                //Modify the event to be processed on sockfd to EPOLIN
-
-                epoll_ctl(epfd,EPOLL_CTL_MOD,sockfd,&ev);
-            }
-        }
-    }
-    return 0;
-}
-#endif
-
-//epoll server with ET and LT dual mode
-#if 0
-
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <string.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <sys/epoll.h>
-#include <pthread.h>
-#include <errno.h>
-#include <stdbool.h>
-
-
-#Maximum number of define MAX_EVENT_NUMBER 1024 //event
-#define BUFFER_SIZE 10 //Buffer Size
-#Define ENABLE_ET 1 //Enable ET mode
-
-/* Set file descriptor to non-congested  */
-int SetNonblocking(int fd)
-{
-    int old_option = fcntl(fd, F_GETFL);
-    int new_option = old_option | O_NONBLOCK;
-    fcntl(fd, F_SETFL, new_option);
-    return old_option;
-}
-
-/* Register EPOLLIN on file descriptor FD into the epoll kernel event table indicated by epoll_fd, and the parameter enable_et specifies whether et mode is enabled for FD */
-void AddFd(int epoll_fd, int fd, bool enable_et)
-{
-    struct epoll_event event;
-    event.data.fd = fd;
-    event.events = EPOLLIN; //Registering the fd is readable
-    if(enable_et)
-    {
-        event.events |= EPOLLET;
-    }
-
-    epoll_ctl(epoll_fd, EPOLL_CTL_ADD, fd, &event);  //Register the fd with the epoll kernel event table
-    SetNonblocking(fd);
-}
-
-/*  LT Work mode features: robust but inefficient */
-void lt_process(struct epoll_event* events, int number, int epoll_fd, int listen_fd)
-{
-    char buf[BUFFER_SIZE];
-    int i;
-    for(i = 0; i < number; i++) //number: number of events ready
-    {
-        int sockfd = events[i].data.fd;
-        if(sockfd == listen_fd)  //If it is a file descriptor for listen, it indicates that a new customer is connected to
-        {
-            struct sockaddr_in client_address;
-            socklen_t client_addrlength = sizeof(client_address);
-            int connfd = accept(listen_fd, (struct sockaddr*)&client_address, &client_addrlength);
-            AddFd(epoll_fd, connfd, false);  //Register new customer connection fd to epoll event table, using lt mode
-        }
-        else if(events[i].events & EPOLLIN) //Readable with client data
-        {
-            // This code is triggered as long as the data in the buffer has not been read.This is what LT mode is all about: repeating notifications until processing is complete
-            printf("lt mode: event trigger once!\n");
-            memset(buf, 0, BUFFER_SIZE);
-            int ret = recv(sockfd, buf, BUFFER_SIZE - 1, 0);
-            if(ret <= 0)  //After reading the data, remember to turn off fd
-            {
-                close(sockfd);
-                continue;
-            }
-            printf("get %d bytes of content: %s\n", ret, buf);
-
-        }
-        else
-        {
-            printf("something unexpected happened!\n");
-        }
-    }
-}
-
-/* ET Work mode features: efficient but potentially dangerous */
-void et_process(struct epoll_event* events, int number, int epoll_fd, int listen_fd)
-{
-    char buf[BUFFER_SIZE];
-    int i;
-    for(i = 0; i < number; i++)
-    {
-        int sockfd = events[i].data.fd;
-        if(sockfd == listen_fd)
-        {
-            struct sockaddr_in client_address;
-            socklen_t client_addrlength = sizeof(client_address);
-            int connfd = accept(listen_fd, (struct sockaddr*)&client_address, &client_addrlength);
-            AddFd(epoll_fd, connfd, true);  //Use et mode
-        }
-        else if(events[i].events & EPOLLIN)
-        {
-            /* This code will not be triggered repeatedly, so we cycle through the data to make sure that all the data in the socket read cache is read out.This is how we eliminate the potential dangers of the ET model */
-
-            printf("et mode: event trigger once!\n");
-            while(1)
-            {
-                memset(buf, 0, BUFFER_SIZE);
-                int ret = recv(sockfd, buf, BUFFER_SIZE - 1, 0);
-                if(ret < 0)
-                {
-                    /* For non-congested IO, the following condition is true to indicate that the data has been read completely, after which epoll can trigger the EPOLLIN event on sockfd again to drive the next read operation */
-
-                    if(errno == EAGAIN || errno == EWOULDBLOCK)
-                    {
-                        printf("read later!\n");
-                        break;
-                    }
-
-                    close(sockfd);
-                    break;
-                }
-                else if(ret == 0)
-                {
-                    close(sockfd);
-                }
-                else //Not finished, continue reading in a loop
-                {
-                    printf("get %d bytes of content: %s\n", ret, buf);
-                }
-            }
-        }
-        else
-        {
-            printf("something unexpected happened!\n");
-        }
-    }
-}
-
-
-int main(int argc, char* argv[])
-{
-    if(argc <= 2)
-    {
-        printf("usage:  ip_address + port_number\n");
-        return -1;
-    }
-
-    const char* ip = argv[1];
-    int port = atoi(argv[2]);
-
-    int ret = -1;
-    struct sockaddr_in address;
-    bzero(&address, sizeof(address));
-    address.sin_family = AF_INET;
-    inet_pton(AF_INET, ip, &address.sin_addr);
-    address.sin_port = htons(port);
-
-    int listen_fd = socket(PF_INET, SOCK_STREAM, 0);
-    if(listen_fd < 0)
-    {
-        printf("fail to create socket!\n");
-        return -1;
-    }
-
-    ret = bind(listen_fd, (struct sockaddr*)&address, sizeof(address));
-    if(ret == -1)
-    {
-        printf("fail to bind socket!\n");
-        return -1;
-    }
-
-    ret = listen(listen_fd, 5);
-    if(ret == -1)
-    {
-        printf("fail to listen socket!\n");
-        return -1;
-    }
-
-    struct epoll_event events[MAX_EVENT_NUMBER];
-    int epoll_fd = epoll_create(5);  //Event table size 5
-    if(epoll_fd == -1)
-    {
-        printf("fail to create epoll!\n");
-        return -1;
-    }
-
-    AddFd(epoll_fd, listen_fd, true); //Add listen file descriptor to event table using ET mode epoll
-
-    while(1)
-    {
-        int ret = epoll_wait(epoll_fd, events, MAX_EVENT_NUMBER, -1);
-        if(ret < 0)
-        {
-            printf("epoll failure!\n");
-            break;
-        }
-
-        if(ENABLE_ET)
-        {
-            et_process(events, ret, epoll_fd, listen_fd);
-        }
-        else
-        {
-            lt_process(events, ret, epoll_fd, listen_fd);
-        }
-
-    }
-
-    close(listen_fd);
-    return 0;
-
-}
-#endif
-
