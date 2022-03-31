e6cd2b543826aee22a08fe35c0fdd9241d293332
Christoph Schueler <cpu12.gems@googlemail.com>
2022-03-31 12:39:08 +0200

    Update CMakeLists.txt files

--- tests/xcp_mocks.c
+++ tests/xcp_mocks.c
@@ -4,6 +4,7 @@
 
 #include "xcp.h"
 
+#if 0
 typedef struct {
     uint8_t value;
     bool down;
@@ -28,17 +29,26 @@ const XcpDaq_ListConfigurationType XcpDaq_PredefinedLists[] = {{2, 0}};
 XcpDaq_ListStateType XcpDaq_PredefinedListsState[XCP_DAQ_PREDEFINDED_LIST_COUNT];
 const XcpDaq_ListIntegerType XcpDaq_PredefinedListCount = XCP_DAQ_PREDEFINDED_LIST_COUNT;
 #endif /* XCP_DAQ_ENABLE_PREDEFINED_LISTS */
+#endif
 
 XCP_DAQ_BEGIN_EVENTS
-XCP_DAQ_DEFINE_EVENT("EVT 100ms", XCP_DAQ_EVENT_CHANNEL_TYPE_DAQ | XCP_DAQ_CONSISTENCY_DAQ_LIST,
+XCP_DAQ_DEFINE_EVENT("EVT 100ms",
+                     XCP_DAQ_EVENT_CHANNEL_TYPE_DAQ |
+                         XCP_DAQ_CONSISTENCY_DAQ_LIST,
                      XCP_DAQ_EVENT_CHANNEL_TIME_UNIT_1MS, 100),
-    XCP_DAQ_DEFINE_EVENT("EVT sporadic", XCP_DAQ_EVENT_CHANNEL_TYPE_DAQ | XCP_DAQ_CONSISTENCY_DAQ_LIST,
+    XCP_DAQ_DEFINE_EVENT("EVT sporadic",
+                         XCP_DAQ_EVENT_CHANNEL_TYPE_DAQ |
+                             XCP_DAQ_CONSISTENCY_DAQ_LIST,
                          XCP_DAQ_EVENT_CHANNEL_TIME_UNIT_1MS, 0),
-    XCP_DAQ_DEFINE_EVENT("EVT 10ms", XCP_DAQ_EVENT_CHANNEL_TYPE_DAQ | XCP_DAQ_CONSISTENCY_DAQ_LIST,
+    XCP_DAQ_DEFINE_EVENT("EVT 10ms",
+                         XCP_DAQ_EVENT_CHANNEL_TYPE_DAQ |
+                             XCP_DAQ_CONSISTENCY_DAQ_LIST,
                          XCP_DAQ_EVENT_CHANNEL_TIME_UNIT_1MS, 10),
     XCP_DAQ_END_EVENTS
 
     uint32_t XcpHw_GetTimerCounter() {
-    //    return clock();
-    return 0xaaaa;
+  //    return clock();
+  return 0xaaaa;
 }
+
+void XcpHw_TransmitDtos(void) {}
