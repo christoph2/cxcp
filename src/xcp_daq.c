681bf8f456380de98f16b9ed04902c972213df7e
Christoph Schueler <cpu12.gems@googlemail.com>
2025-11-01 16:05:54 +0200

    Fix XcpDaq_TransmitDtos()

--- src/xcp_daq.c
+++ src/xcp_daq.c
@@ -807,12 +807,14 @@ void XcpDaq_StartStopSynch(uint8_t mode) {
 }
 
 void XcpDaq_TransmitDtos(void) {
-    uint16_t len;
+    uint16_t len=0UL;
     uint8_t *dataOut = Xcp_GetDtoOutPtr();
+
     while (!XcpDaq_QueueEmpty()) {
-        XcpDaq_QueueDequeue(&len, dataOut);
-        Xcp_SetDtoOutLen(len);
-        Xcp_SendDto();
+        if (XcpDaq_QueueDequeue(&len, dataOut)) {
+            Xcp_SetDtoOutLen(len);
+            Xcp_SendDto();
+        }
     }
 }
 
